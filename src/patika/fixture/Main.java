2b04018ab3884e82d6d603c28eb8d18b5f1ea27b
Kadir Erkan <87225704+kadirterkan@users.noreply.github.com>
2022-03-16 15:40:25 +0300

    master
    Please kill me.

--- src/patika/fixture/Main.java
+++ src/patika/fixture/Main.java
@@ -8,32 +8,43 @@ public class Main {
     private final static SplittableRandom random = new SplittableRandom();
 
     public static void main(String[] args) {
+        List<String> teamListEven = returnEven();
+        int teamCountEven = teamListEven.size();
 
-//        System.out.println("Takım sayısını giriniz");
-
-        Integer teamCount = 6;
-
-//        System.out.println("Takımları giriniz");
-
-        List<String> teamList = new ArrayList<>();
-        teamList.add("Galatasaray");
-        teamList.add("Bursaspor");
-        teamList.add("Fenerbahçe");
-        teamList.add("Beşiktaş");
-        teamList.add("Başakşehir");
-        teamList.add("Trabzonspor");
-
-
-//            for (int i = 0; i < teamCount; i++) {
-//                teamList.add(input.nextLine());
-//            }
-
-        Integer weekCount = 2 * (teamCount - 1);
-
-        if (teamCount % 2 != 0) {
-            teamList.add("Bay");
+        if (teamCountEven % 2 != 0) {
+            teamListEven.add("Bay");
+            teamCountEven++;
         }
 
+        LinkedHashSet<Match<String, String>> evenList = shuffleThis(teamListEven, teamCountEven);
+
+        int weekCountEven = 2 * (teamCountEven - 1);
+
+        printList(evenList, weekCountEven, teamCountEven);
+
+        List<String> teamListOdd = returnOdd();
+        int teamCountOdd = teamListOdd.size();
+
+        if (teamCountOdd % 2 != 0) {
+            teamListOdd.add("Bay");
+            teamCountOdd++;
+        }
+
+        System.out.println();
+        System.out.println("***************************");
+        System.out.println("***************************");
+        System.out.println("***************************");
+        System.out.println("2. Sezon");
+        LinkedHashSet<Match<String, String>> oddList = shuffleThis(teamListOdd, teamCountOdd);
+
+        int weekCountOdd = 2 * (teamCountOdd - 1);
+
+        printList(oddList, weekCountOdd, teamCountOdd);
+
+
+    }
+
+    public static LinkedHashSet<Match<String, String>> shuffleThis(List<String> teamList, int teamCount) {
         LinkedHashSet<Match<String, String>> matches = new LinkedHashSet<>();
 
         Integer totalMatchCount = (teamCount) * (teamCount - 1);
@@ -52,6 +63,35 @@ public class Main {
             }
         }
 
+        return matches;
+    }
+
+    public static List<String> returnEven() {
+        List<String> teamList = new ArrayList<>();
+        teamList.add("Galatasaray");
+        teamList.add("Bursaspor");
+        teamList.add("Fenerbahçe");
+        teamList.add("Beşiktaş");
+        teamList.add("Başakşehir");
+        teamList.add("Trabzonspor");
+
+        return teamList;
+    }
+
+    public static List<String> returnOdd() {
+        List<String> teamList = new ArrayList<>();
+        teamList.add("Galatasaray");
+        teamList.add("Bursaspor");
+        teamList.add("Fenerbahçe");
+        teamList.add("Beşiktaş");
+        teamList.add("Başakşehir");
+        teamList.add("Bursaspor");
+        teamList.add("Boluspor");
+
+        return teamList;
+    }
+
+    public static void printList(LinkedHashSet<Match<String, String>> matches, int weekCount, int teamCount) {
         System.out.println("Week 1");
         int count = 1;
         for (Match match : matches) {
